93eb679f6aea92b3be22cc784f1a98a8785dfc70
dawnliang <dawn7liang@gmail.com>
2016-10-12 10:49:41 -0700

    counter updates

---
+++ lab1/counters/johnsonUpCounter.v
@@ -0,0 +1,32 @@
+module johnsonUpCounter(out, clk, rst);
+	output logic [3:0] out;
+	input logic clk, rst;
+	logic [3:0] temp; 
+	
+	always_ff@(negedge rst or posedge clk) begin
+		if (rst == 0)
+		begin
+			temp <= 4'b0000;
+		end
+		else if (clk == 1'b1)
+		begin
+			temp <= {~temp[0], temp[3:1]}; // right shift and negate most signf bit
+		end	
+	end
+
+	assign out = temp; 
+endmodule
+
+module DFlipFlop(q, qBar, D, clk, rst);
+	input D, clk, rst;
+	output q, qBar;
+	reg q;
+	
+	not n1 (qBar, q);
+	always@ (negedge rst or posedge clk) begin
+		if(!rst)
+			q = 0;
+		else
+			q = D;
+	end
+endmodule
---
+++ lab1/iverilog&gtkwave/testBench.v
@@ -0,0 +1,22 @@
+module testBench;
+	// connect the two modules
+	wire inputs;
+	wire outputs;
+
+	// declare an instance of the MyDesign module MyDesign
+	myDesign(outputs, inputs);
+
+	// declare an instance of the Tester module Tester
+	myTester (outputs, inputs);
+
+	// file specifications for gtkwave
+	initial
+	begin
+		// dump file is for dumping all the variables in a simulation
+		$dumpfile("gfxFile.vcd");
+
+		// dumps all the variables in module myDesign and below
+		// but not modules instantiated in myDesign into the dump file.
+		$dumpvars(1,myDesign);
+	end
+endmodule
