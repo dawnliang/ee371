0b946553e157f6413275eb561108f22b05d0ae66
dawnliang <dawn7liang@gmail.com>
2016-10-13 12:39:33 -0700

    clk divider and c project

---
+++ lab1/board_clk/clk_divider.v
@@ -0,0 +1,74 @@
+module clk_divider(clk, clk_out);
+	input clk;
+	output clk_out;
+	reg [63:0] tBase;
+
+	always @(posedge clk)		// time base for system
+	begin
+		tBase <= tBase + 1'b1;
+	end
+
+	always @(posedge tBase[44])	// select a frequency bit
+	begin
+		clk_out <= tBase[44];
+	end
+endmodule
+
+module clk_divider_testbench;
+	logic clk, out;
+
+	clk_divider dut(.clk, .clk_out(out));
+
+	initial begin
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+	end
+endmodule
