0b97819f11f07e66570470a17541e1d4af1f8fc9
dawnliang <dawn7liang@gmail.com>
2016-11-13 14:57:46 -0800

    lab complete

---
+++ lab3/quartus/verilog/counter.v.bak
@@ -0,0 +1,34 @@
+// EE371 Lab3 Autumn 2016
+// Authors: Dawn Liang, Jun Park, William Li
+// Date:
+//
+// counter that counts every clock edge up/down to a min/max (0/10)
+module counter(val, up, down, clk, reset);
+	output [3:0] val;
+	input up, down, clk, reset;
+
+	// combinational logic: count every clock edge
+	// min/max set at 0/10
+	reg [3:0] ps, ns;
+	always@(*) begin
+		if (up && ~down && (ps < 4'b1010)) begin
+			ns = ps + 1'b1;
+		end else if (down && ~up && (ps > 4'b0)) begin
+			ns = ps - 1'b1;
+		end else begin
+			ns = ps;
+		end
+	end
+
+	// output logic
+	assign val = ps;
+	
+	// sequential logic; reset & next state
+	always@(posedge clk) begin
+		if (reset) begin
+			ps <= 4'b0;
+		end else begin
+			ps <= ns;
+		end
+	end
+endmodule
---
+++ lab3/quartus/verilog/scanner.v
@@ -0,0 +1,20 @@
+module scanner(buffer_progress, state, readyToTransfer, otherGoToStandby, otherStartScan, otherFlush,
+				whichScanner, initialOn, goToStandby, startScan, startTransfer, flush, clk, reset);
+	input clk, reset;
+	input initialOn, goToStandby, startScan, startTransfer, flush, whichScanner;
+
+	output readyToTransfer, otherGoToStandby, otherStartScan, otherFlush;
+	output [3:0] buffer_progress;
+	output [2:0] state;
+	
+	parameter scanning = 3'b010;
+
+	scannerState statedet (.state(state), .whichScanner(whichScanner), .initialOn(initialOn), .goToStandby(goToStandby), .startScan(startScan),
+		.prog(buffer_progress), .startTransfer(startTransfer), .flush(flush), .clk(clk), .reset(reset));
+	counterCtrl prog (.val(buffer_progress), .state(state), .clk(clk), .reset(reset));
+
+	assign readyToTransfer = (buffer_progress == 5 && state == scanning);
+	assign otherGoToStandby = (buffer_progress == 8 && state == scanning);
+	assign otherStartScan = (buffer_progress == 9 && state == scanning);
+	assign otherFlush = (buffer_progress == 5 && state == scanning);
+endmodule
